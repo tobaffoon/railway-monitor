db4c5c692e891b7ce73006121396ebf73eb17300
Мазяр Алексей Николаевич <a.mazyar@g.nsu.ru>
2023-12-12 23:51:17 +0700

    Add simple containers and bindings for tools

---
+++ MVVM/ViewModels/ToolButtonsViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace railway_monitor.MVVM.ViewModels
+{
+    class ToolButtonsViewModel : DependencyObject
+    {
+        private static readonly DependencyPropertyKey ToolButtonsPropertyKey;
+        public static readonly DependencyProperty ToolButtonsProperty;
+
+        static ToolButtonsViewModel()
+        {
+
+            List<RadioButton> toolButtons = new List<RadioButton>();
+
+            RadioButton srtButton = new RadioButton();
+            srtButton.GroupName = "Tools";
+            srtButton.Content = "SRT";
+            RadioButton switchButton = new RadioButton();
+            switchButton.GroupName = "Tools";
+            switchButton.Content = "Switch";
+
+            toolButtons.Add(srtButton);
+            toolButtons.Add(switchButton);
+
+            ToolButtonsPropertyKey =
+                DependencyProperty.RegisterReadOnly(
+                    "ToolButtons",
+                    typeof(List<RadioButton>),
+                    typeof(ToolButtonsViewModel),
+                    new FrameworkPropertyMetadata(toolButtons)
+                );
+
+            ToolButtonsProperty = ToolButtonsPropertyKey.DependencyProperty;
+        }
+
+        public List<RadioButton> ToolButtons => (List<RadioButton>)GetValue(ToolButtonsProperty);
+    }
+}
