aa14974cd9e4f9e62b2134ebe19eb05b50cc1ffd
Мазяр Алексей Николаевич <a.mazyar@g.nsu.ru>
2023-12-13 10:18:32 +0700

    Add rest of tools with hard code

--- MVVM/ViewModels/ToolButtonsViewModel.cs
+++ MVVM/ViewModels/ToolButtonsViewModel.cs
@@ -19,15 +19,37 @@ namespace railway_monitor.MVVM.ViewModels
 
             List<RadioButton> toolButtons = new List<RadioButton>();
 
-            RadioButton srtButton = new RadioButton();
-            srtButton.GroupName = "Tools";
-            srtButton.Content = "SRT";
-            RadioButton switchButton = new RadioButton();
-            switchButton.GroupName = "Tools";
-            switchButton.Content = "Switch";
+            RadioButton srtButton = new RadioButton
+            {
+                GroupName = "Tools",
+                Content = "SRT"
+            };
+            RadioButton switchButton = new RadioButton
+            {
+                GroupName = "Tools",
+                Content = "Switch"
+            };
+            RadioButton signalButton = new RadioButton
+            {
+                GroupName = "Tools",
+                Content = "Signal"
+            };
+            RadioButton deadEndButton = new RadioButton
+            {
+                GroupName = "Tools",
+                Content = "Dead-end"
+            };
+            RadioButton externalTrackButton = new RadioButton
+            {
+                GroupName = "Tools",
+                Content = "External Track"
+            };
 
             toolButtons.Add(srtButton);
             toolButtons.Add(switchButton);
+            toolButtons.Add(signalButton);
+            toolButtons.Add(deadEndButton);
+            toolButtons.Add(externalTrackButton);
 
             ToolButtonsPropertyKey =
                 DependencyProperty.RegisterReadOnly(
