5e5cde8f3e9eb1f01001f3071b9b0e8ce6bff37c
Мазяр Алексей Николаевич <a.mazyar@g.nsu.ru>
2023-12-19 18:12:52 +0700

    Add empty main ViewModels

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using railway_monitor.MVVM.ViewModels;
 
 namespace railway_monitor
 {
@@ -11,7 +12,10 @@ namespace railway_monitor
     {
         protected override void OnStartup(StartupEventArgs e)
         {
-            MainWindow = new MainWindow();
+            MainWindow = new MainWindow()
+            {
+                DataContext = new MainViewModel()
+            };
             MainWindow.Show();
 
             base.OnStartup(e);
---
+++ MVVM/ViewModels/RailwayMonitorViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace railway_monitor.MVVM.ViewModels
+{
+    class RailwayMonitorViewModel : ViewModelBase
+    {
+
+    }
+}
