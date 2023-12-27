f2511ca2b452316cd1f31e706107a10663147f66
Тимур Ильиных <55120919+m1Myp@users.noreply.github.com>
2023-12-27 09:41:53 +0700

    fintal 1s sem commit

--- MVVM/ViewModels/RailwayMonitorViewModel.cs
+++ MVVM/ViewModels/RailwayMonitorViewModel.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System.Windows;
 using railway_monitor.Tools;
 
@@ -10,3 +11,26 @@ namespace railway_monitor.MVVM.ViewModels
         }
     }
 }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using railway_monitor.Components.ToolButtons;
+using railway_monitor.Tools;
+
+namespace railway_monitor.MVVM.ViewModels
+{
+    public class RailwayMonitorViewModel : ViewModelBase
+    {
+        public RailwayMonitorViewModel()
+        {
+        }
+    }
+}
+>>>>>>> fintal 1s sem commit
