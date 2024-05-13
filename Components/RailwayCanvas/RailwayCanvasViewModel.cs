00244e5b84b7e1a4128a19817214946aea7ab656
Мазяр Алексей Николаевич <a.mazyar@g.nsu.ru>
2024-05-14 01:41:57 +0700

    Add 1v srt connection

--- Components/RailwayCanvas/RailwayCanvasViewModel.cs
+++ Components/RailwayCanvas/RailwayCanvasViewModel.cs
@@ -7,11 +7,18 @@ using System.Windows.Shapes;
 using railway_monitor.Utils;
 using railway_monitor.MVVM.ViewModels;
 using System.Windows;
+using System.Windows.Diagnostics;
+using System.Windows.Media;
+using railway_monitor.Components.GraphicItems;
+using System.Diagnostics;
 
 namespace railway_monitor.Components.RailwayCanvas
 {
     public class RailwayCanvasViewModel : ViewModelBase
     {
+        private static readonly double _connectRadius = 10;
+        private StraightRailTrack? ConnectionTrack {  get; set; }
+
         public ObservableHashSet<Shape> GraphicItems { get; } = new ObservableHashSet<Shape>();
         public Shape? LatestShape { get; set; }
         public int Len { get { return GraphicItems.Count; } }
@@ -36,9 +43,40 @@ namespace railway_monitor.Components.RailwayCanvas
             LatestShape = null;
         }
 
-        // TODO
+        private HitTestResultBehavior RailHitTestResult(HitTestResult result)
+        {
+            if (result.VisualHit != LatestShape)
+            {
+                ConnectionTrack = result.VisualHit as StraightRailTrack;
+                return HitTestResultBehavior.Stop;
+            }
+            else
+            {
+                return HitTestResultBehavior.Continue;
+            }
+        }
+
         public Point TryFindRailConnection(Point mousePos)
         {
+            EllipseGeometry expandedHitTestArea = new EllipseGeometry(mousePos, _connectRadius, _connectRadius);
+            foreach (StraightRailTrack srt in GraphicItems.OfType<StraightRailTrack>())
+            {
+                ConnectionTrack = null;
+                VisualTreeHelper.HitTest(srt, 
+                    null,
+                    new HitTestResultCallback(RailHitTestResult),
+                    new GeometryHitTestParameters(expandedHitTestArea));
+                if (ConnectionTrack == null) continue; 
+
+                // determine close enough vertex or make sure that there is no such vertex
+                double distance1 = (ConnectionTrack.Start - mousePos).Length;
+                double distance2 = (ConnectionTrack.End - mousePos).Length;
+                if (distance1 < _connectRadius || distance2 < _connectRadius)
+                {
+                    if (distance2 < distance1) return ConnectionTrack.End;
+                    return ConnectionTrack.Start;
+                }
+            }
             return mousePos;
         }
     }
