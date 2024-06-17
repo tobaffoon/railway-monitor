21888d8566df2dc13f571d3adc8c09f8fd163254
Мазяр Алексей Николаевич <a.mazyar@g.nsu.ru>
2024-06-17 15:04:26 +0700

    Fix: not srt items placed in one port
    Change connection conditions

--- Utils/ConnectConditions.cs
+++ Utils/ConnectConditions.cs
@@ -10,17 +10,21 @@ namespace railway_monitor.Utils {
             if (switches != 0 && srts >= 3) return false;
             return true;
         }
+        private static bool OtherItemsPresent(Port connectionPort) {
+            int srtItems = connectionPort.GraphicItems.OfType<StraightRailTrackItem>().Count();
+            return connectionPort.GraphicItems.Count() - srtItems > 0;
+        }
         public static bool IsSwitchConnectable(Port connectionPort) {
-            return connectionPort.GraphicItems.OfType<StraightRailTrackItem>().Count() == 3 && connectionPort.GraphicItems.OfType<SwitchItem>().Count() == 0;
+            return connectionPort.GraphicItems.OfType<StraightRailTrackItem>().Count() == 3 && !OtherItemsPresent(connectionPort);
         }
         public static bool IsSignalConnectable(Port connectionPort) {
-            return connectionPort.GraphicItems.OfType<StraightRailTrackItem>().Count() >= 2 && connectionPort.GraphicItems.OfType<SignalItem>().Count() == 0;
+            return connectionPort.GraphicItems.OfType<StraightRailTrackItem>().Count() >= 2 && !OtherItemsPresent(connectionPort);
         }
         public static bool IsExternalTrackConnectable(Port connectionPort) {
-            return connectionPort.GraphicItems.OfType<ExternalTrackItem>().Count() == 0;
+            return !OtherItemsPresent(connectionPort);
         }
         public static bool IsDeadendConnectable(Port connectionPort) {
-            return connectionPort.GraphicItems.OfType<StraightRailTrackItem>().Count() == 1 && connectionPort.GraphicItems.OfType<DeadendItem>().Count() == 0;
+            return connectionPort.GraphicItems.OfType<StraightRailTrackItem>().Count() == 1 && !OtherItemsPresent(connectionPort);
         }
         public static bool RailHasPlatform(StraightRailTrackItem srt) {
             return srt.PlatformType == StraightRailTrackItem.RailPlatformType.PASSENGER || srt.PlatformType == StraightRailTrackItem.RailPlatformType.CARGO;
