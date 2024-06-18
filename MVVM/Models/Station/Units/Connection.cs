737f6c8a4325b8c44f7d689a3a702417cf426fa8
Мазяр Алексей Николаевич <a.mazyar@g.nsu.ru>
2024-06-18 23:00:05 +0700

    Remove shtupid useless models

--- MVVM/Models/Station/Units/Connection.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace railway_monitor.MVVM.Models.Station.Units {
-    /// <summary>
-    /// Connection between 2 SRTs. Doesn't do anything, just allows polyline tracks.
-    /// </summary>
-    public class Connection : NodeUnit {
-    }
-}
