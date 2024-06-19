1ed38ff441a4cdb7384b87170a1f5945378d1f7b
Мазяр Алексей Николаевич <a.mazyar@g.nsu.ru>
2024-06-19 22:22:08 +0700

    Add: pen and brush for broken station units

--- Bases/TopologyItem.cs
+++ Bases/TopologyItem.cs
@@ -1,5 +1,10 @@
-﻿namespace railway_monitor.Bases {
+﻿using System.Windows.Media;
+
+namespace railway_monitor.Bases {
     public abstract class TopologyItem : GraphicItem {
+        protected static readonly Brush brokenBrush = new SolidColorBrush(Color.FromArgb(200, 234, 67, 53));
+        protected static readonly Pen brokenPen = new Pen(brokenBrush, 0.5);
+
         public abstract void Reassign_OnPortMerged(object? sender, Port oldPort);
     }
 }
