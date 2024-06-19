3d1e1c25123b8a1ae41bb06984f6db7a4566403e
Мазяр Алексей Николаевич <a.mazyar@g.nsu.ru>
2024-06-19 18:50:28 +0700

    Fix: move trainManager and rename to station

--- MVVM/Models/Station/StationManager.cs
+++ MVVM/Models/Station/StationManager.cs
@@ -6,15 +6,18 @@ using railway_monitor.Utils;
 using SolverLibrary.Model.Graph;
 using SolverLibrary.Model.Graph.VertexTypes;
 
-namespace railway_monitor.Trains {
-    public class TrainManager {
+namespace railway_monitor.MVVM.Models.Station
+{
+    public class StationManager
+    {
         private RailwayCanvasViewModel canvas;
         private StationGraph stationGraph;
         private Dictionary<Vertex, TopologyItem> topologyVertexDict;
         private Dictionary<Edge, StraightRailTrackItem> topologyEdgeDict;
         private Dictionary<int, TrainItem> trains;
 
-        public TrainManager(RailwayCanvasViewModel canvas) {
+        public StationManager(RailwayCanvasViewModel canvas)
+        {
             this.canvas = canvas;
             Tuple<StationGraph, Dictionary<Vertex, TopologyItem>, Dictionary<Edge, StraightRailTrackItem>> graphInfo = GraphUtils.CreateGraph(canvas.Rails);
             stationGraph = graphInfo.Item1;
