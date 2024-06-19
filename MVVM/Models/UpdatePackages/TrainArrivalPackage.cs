449d951eca4d07d65eddf8137d82808fe85a0813
Мазяр Алексей Николаевич <a.mazyar@g.nsu.ru>
2024-06-19 19:34:56 +0700

    Rename arrive to arrival package

--- MVVM/Models/UpdatePackages/TrainArrivalPackage.cs
+++ MVVM/Models/UpdatePackages/TrainArrivalPackage.cs
@@ -1,9 +1,9 @@
 ﻿namespace railway_monitor.MVVM.Models.UpdatePackages {
-    public readonly struct TrainArrivePackage {
+    public readonly struct TrainArrivalPackage {
         public readonly int trainId;
         public readonly int inputVertexId;
 
-        public TrainArrivePackage(int trainId, int inputVertexId) {
+        public TrainArrivalPackage(int trainId, int inputVertexId) {
             this.trainId = trainId;
             this.inputVertexId = inputVertexId;
         }
