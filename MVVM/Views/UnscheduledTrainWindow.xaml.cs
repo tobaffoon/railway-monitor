cb5cf09923476cb79e86d99ed94ce54e753e2b3a
Мазяр Алексей Николаевич <a.mazyar@g.nsu.ru>
2024-06-20 20:23:43 +0700

    Add: empty dialog window for unscheduled trains

---
+++ MVVM/Views/UnscheduledTrainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace railway_monitor.MVVM.Views {
+    /// <summary>
+    /// Interaction logic for UnscheduledTrainWindows.xaml
+    /// </summary>
+    public partial class UnscheduledTrainWindows : Window {
+        public UnscheduledTrainWindows() {
+            InitializeComponent();
+        }
+
+        private void ChooseUnscheduledTrack(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void ScheduleNewTrain(object sender, RoutedEventArgs e) {
+
+        }
+    }
+}
