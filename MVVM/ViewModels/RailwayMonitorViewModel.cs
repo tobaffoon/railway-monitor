ea63ea30244a16ca641287f95cfd7111eb1d3ab0
Мазяр Алексей Николаевич <a.mazyar@g.nsu.ru>
2024-06-23 20:14:09 +0700

    Add: current time in monitor view

--- MVVM/ViewModels/RailwayMonitorViewModel.cs
+++ MVVM/ViewModels/RailwayMonitorViewModel.cs
@@ -9,6 +9,16 @@ namespace railway_monitor.MVVM.ViewModels {
     public class RailwayMonitorViewModel : RailwayBaseViewModel { 
         private static readonly int _defaultTimeInaccuracy = 5;
         public StationManager? StationManager { get; private set; }
+        public string CurrentTime {
+            get {
+                if (StationManager == null) {
+                    return "0 s";
+                }
+                else {
+                    return StationManager.CurrentTime + " s";
+                }
+            }
+        }
 
         public RailwayMonitorViewModel(MainViewModel mainViewModel) : base(mainViewModel) {
             CanvasKeyboardCommand = new KeyboardCommand(UtilToolActions.NoKeyboardAction);
